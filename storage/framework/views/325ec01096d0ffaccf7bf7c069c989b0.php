71d3dc976d21c39d53c07a13687ed0e342fce39f
Vyan <88295018+OctaviantoVyan@users.noreply.github.com>
2024-01-14 17:28:40 +0800

    Add files via upload

---
+++ storage/framework/views/325ec01096d0ffaccf7bf7c069c989b0.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Languange\2024\vue3_lara_10\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
